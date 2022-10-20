3b20113c39f01bb04d37ab34d7229571f49790fa
paeria <kawaokakouta@outlook.jp>
2022-10-20 23:14:20 +0900

    上書き

--- hello-server/main.go
+++ hello-server/main.go
@@ -7,6 +7,12 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
+type jsonData struct {
+	Number int
+	String string
+	Bool   bool
+}
+
 func main() {
 	e := echo.New()
 
@@ -16,5 +22,18 @@ func main() {
 	e.GET("/hello", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World.\n")
 	})
+
+	e.GET("/json", jsonHandler)
+
 	e.Start(":8080")
+}
+
+func jsonHandler(c echo.Context) error {
+	res := jsonData{
+		Number: 10,
+		String: "hoge",
+		Bool:   false,
+	}
+
+	return c.JSON(http.StatusOK, &res)
 }

a1a2133dd91e05d161b2f3c4449b23a2903e2497
paeria <kawaokakouta@outlook.jp>
2022-10-22 19:24:10 +0900

    サーバー更新

--- hello-server/main.go
+++ hello-server/main.go
@@ -19,21 +19,12 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	e.GET("/hello", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World.\n")
-	})
-
-	e.GET("/json", jsonHandler)
+	e.POST("/hello/:name", helloHandler)
 
 	e.Start(":8080")
 }
 
-func jsonHandler(c echo.Context) error {
-	res := jsonData{
-		Number: 10,
-		String: "hoge",
-		Bool:   false,
-	}
-
-	return c.JSON(http.StatusOK, &res)
+func helloHandler(c echo.Context) error {
+	name := c.Param("name")
+	return c.String(http.StatusOK, "Hello, "+name+".\n")
 }
